54109394db281d0c8f857afc565067742988a9d5
boredazfcuk <boredazfcuk@hotmail.co.uk>
2020-02-09 23:07:08 +0000

    Should be a change log. If not, I've made so many updates I've forgotten, or too few to bother updating it

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ARG app_dependencies="shadow apache2-utils"
 ENV config_dir="/etc/nginx" 
 
 RUN echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD STARTED *****" && \
+   mkdir "${config_dir}/locations/" && \
    echo "$(date '+%d/%m/%Y - %H:%M:%S') | Install dependencies" && \
    apk add --no-cache --no-progress ${app_dependencies} && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | Add nginx GeoIP module" && \
@@ -11,20 +12,16 @@ echo "$(date '+%d/%m/%Y - %H:%M:%S') | Add nginx GeoIP module" && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | Backup default nginx config" && \
    mv -v "/usr/share/nginx/html" "/usr/share/nginx/html.default" && \
    mv -v "${config_dir}/nginx.conf" "${config_dir}/nginx.conf.default" && \
-   mv -v "${config_dir}/conf.d/default.conf" "${config_dir}/conf.d/default.conf.default" && \
-echo "$(date '+%d/%m/%Y - %H:%M:%S') | Configure logging" && \
-   ln -sfv /var/log/nginx/access.log /dev/stdout && \
-   ln -sfv /var/log/nginx/error.log /dev/stderr
+   mv -v "${config_dir}/conf.d/default.conf" "${config_dir}/conf.d/default.conf.default"
 
 COPY nginx/* "${config_dir}/"
 COPY conf.d/* "${config_dir}/conf.d/"
-COPY html/* "${config_dir}/html/"
 COPY locations/* "${config_dir}/locations/"
-COPY start-nginx.sh /usr/local/bin/start-nginx.sh
+COPY entrypoint.sh /usr/local/bin/entrypoint.sh
 COPY healthcheck.sh /usr/local/bin/healthcheck.sh
 
 RUN echo  "$(date '+%d/%m/%Y - %H:%M:%S') | Set permissions on scripts" && \
-   chmod +x /usr/local/bin/start-nginx.sh /usr/local/bin/healthcheck.sh && \
+   chmod +x /usr/local/bin/entrypoint.sh /usr/local/bin/healthcheck.sh && \
 echo "$(date '+%d/%m/%Y - %H:%M:%S') | ***** BUILD COMPLETE *****"
 
 HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
@@ -32,4 +29,4 @@ HEALTHCHECK --start-period=10s --interval=1m --timeout=10s \
 
 VOLUME "${config_dir}" /var/cache/nginx/
 
-CMD /usr/local/bin/start-nginx.sh
+ENTRYPOINT "/usr/local/bin/entrypoint.sh"
---
+++ entrypoint.sh
@@ -0,0 +1,309 @@
+#!/bin/ash
+
+##### Functions #####
+Initialise(){
+   echo -e "\n"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Configuring NGINX container launch environment *****"
+   docker_lan_ip="$(hostname -i)"
+   docker_lan_ip_subnet="$(ip -4 route | grep "${lan_ip}" | grep -v via | awk '{print $1}')"
+   {
+      echo "allow ${nginx_lan_ip_subnet};"
+      echo "allow ${docker_lan_ip_subnet};"
+   } > /etc/nginx/local_networks.conf
+   if [ ! -f "/usr/share/GeoIP/GeoIP.dat" ]; then echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** GeoIP Country database does not exist, waiting for it to be created ****"; while [ ! -f "/usr/share/GeoIP/GeoIP.dat" ]; do sleep 2; done; fi
+   if [ "${group_id}" ]; then
+      if [ "$(grep -c ":{group_id}:{group_id}:" /etc/passwd)" = 0 ]; then
+         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Change Group ID to: ${group_id}"
+         groupmod -o nginx -g "${group_id}"
+      fi
+   fi
+   if [ "${user_id}" ]; then
+      if [ "$(grep -c ":{user_id}:{user_id}:" /etc/passwd)" = 0 ]; then
+         echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Change User ID to: ${user_id}"
+         usermod -o nginx -u "${user_id}"
+      fi
+   fi
+   if [ "${media_access_domain}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Media access domain name: ${media_access_domain}"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') ERROR:   Media access domain name not set, cannot continue"
+      sleep 60
+      exit 1
+   fi
+   if [ "${nextcloud_access_domain}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Nextcloud access domain name: ${nextcloud_access_domain}"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Nextcloud access domain name: not configured"
+   fi
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Username: ${stack_user:=stackman}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Password: ${stack_password:=Skibidibbydibyodadubdub}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Host LAN IP subnet: ${nginx_lan_ip_subnet}"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Docker LAN IP subnet: ${docker_lan_ip_subnet}"
+}
+
+DownloadFavouritesIcon(){
+   if [ ! -f "/etc/nginx/html/favicon.ico" ]; then
+      if [ ! -d "/etc/nginx/html" ]; then mkdir "/etc/nginx/html"; fi
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Favicon.ico missing. Downloading from iconfinder.com"
+      wget -q "https://www.iconfinder.com/icons/81001/download/ico/64" -O "/tmp/temp.ico"
+      mv "/tmp/temp.ico" "/etc/nginx/html/favicon.ico"
+   fi
+}
+
+WebProxy(){
+   if [ ! -f "/etc/nginx/html/proxy.pac" ]; then
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Create default proxy.pac"
+   {
+      echo 'function FindProxyForURL(url, host) {'
+      echo '   // Sends local network traffic direct'
+      echo '   if ('
+      echo '      isPlainHostName(host) ||'
+      echo '   )  return "DIRECT";'
+      echo '   // Send all other traffic direct'
+      echo '   return "DIRECT";'
+      echo '}'
+   } > /etc/nginx/html/proxy.pac
+   fi
+}
+
+SetPassword(){
+   if [ ! -f "/etc/nginx/users.htpasswd" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Creating users.htpasswd file and adding user ${stack_user}"
+      htpasswd -bc "/etc/nginx/users.htpasswd" "${stack_user}" "${stack_password}" >/dev/null 2>&1
+   fi
+   if [ -f "/etc/nginx/users.htpasswd" ] && [ "$(grep -c ${stack_user} /etc/nginx/users.htpasswd)" = 0 ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: users.htpasswd file exists but does not contain current user. Removing and recreating password file"
+      rm "/etc/nginx/users.htpasswd"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') WARNING: Creating users.htpasswd file and adding user ${stack_user}"
+      htpasswd -bc "/etc/nginx/users.htpasswd" "${stack_user}" "${stack_password}" >/dev/null 2>&1
+   fi
+}
+
+ConfigureServerNames(){
+   if [ -z "${nextcloud_access_domain}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting default HTTP server to respond on ${media_access_domain} "
+      sed -i \
+         -e "s%server_name .*%server_name ${media_access_domain};%" \
+         /etc/nginx/conf.d/http.conf
+   elif [ "${nextcloud_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting default HTTP server to respond on ${media_access_domain} and ${nextcloud_access_domain}"
+      sed -i \
+         -e "s%server_name .*%server_name ${media_access_domain} ${nextcloud_access_domain};%" \
+         /etc/nginx/conf.d/http.conf
+   fi
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting media server to respond on ${media_access_domain}"
+   sed -i \
+      -e "s%server_name .*$%server_name ${media_access_domain};%" \
+      /etc/nginx/conf.d/media.conf
+   if [ "${nextcloud_enabled}" ] && [ "${nextcloud_access_domain}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting Nextcloud server to respond on ${nextcloud_access_domain}"
+      sed -i \
+         -e "s%server_name .*%server_name ${nextcloud_access_domain};%" \
+         /etc/nginx/conf.d/nextcloud.conf
+   fi
+}
+
+ConfigureCertificates(){
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting certificate file location for ${media_access_domain}"
+   sed -i \
+      -e "s%/etc/letsencrypt/live/.*/%/etc/letsencrypt/live/${media_access_domain}/%g" \
+      /etc/nginx/certificates.conf
+   if [ "${nextcloud_access_domain}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Setting certificate file location for ${nextcloud_access_domain}"
+      sed -i \
+         -e "s%/etc/letsencrypt/live/.*/%/etc/letsencrypt/live/${nextcloud_access_domain}/%g" \
+         /etc/nginx/nextcloud_certificates.conf
+   fi
+}
+
+ConfigureNextcloud(){
+   if [ "${nextcloud_enabled}" ]; then
+      if [ "${nextcloud_access_domain}" ]; then
+         sed -i \
+            -e "s%^   #include /etc/nginx/conf.d/nextcloud.conf;%   include /etc/nginx/conf.d/nextcloud.conf;%" \
+            /etc/nginx/nginx.conf
+         sed -i \
+            -e "s%^   include /etc/nginx/locations/nextcloud.conf;%   #include /etc/nginx/locations/nextcloud.conf;%" \
+            /etc/nginx/conf.d/media.conf
+      else
+         sed -i \
+            -e "s%^   include /etc/nginx/conf.d/nextcloud.conf;%   #include /etc/nginx/conf.d/nextcloud.conf;%" \
+            /etc/nginx/nginx.conf
+         sed -i \
+            -e "s%^   #include /etc/nginx/locations/nextcloud.conf;%   include /etc/nginx/locations/nextcloud.conf;%" \
+            /etc/nginx/conf.d/media.conf
+      fi
+   else
+      sed -i \
+         -e "s%^   include /etc/nginx/conf.d/nextcloud.conf;%   #include /etc/nginx/conf.d/nextcloud.conf;%" \
+         /etc/nginx/conf.d/http.conf
+      sed -i \
+         -e "s%^   include /etc/nginx/conf.d/nextcloud.conf;%   #include /etc/nginx/conf.d/nextcloud.conf;%" \
+         /etc/nginx/nginx.conf
+   fi
+}
+
+LANLogging(){
+   if [ "${nginx_lan_logging}" = "True" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Log traffic from local networks"
+      echo 'access_log  /var/log/nginx/access.log main;' > /etc/nginx/logging.conf
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Exclude networks from logging: ${nginx_lan_ip_subnet}, ${docker_lan_ip_subnet}"
+      {
+         echo 'map $remote_addr $ignore_ips {'
+         echo "   ${nginx_lan_ip_subnet} 1;"
+         echo "   ${docker_lan_ip_subnet} 1;"
+         echo '   default 0;'
+         echo '}'
+         echo 'access_log  /var/log/nginx/access.log main if=$ignore_ips;'
+      } > /etc/nginx/logging.conf
+   fi
+}
+
+Xenophobia(){
+   if [ "${nginx_xenophobia}" ] && [ -f "/usr/share/GeoIP/GeoIP.dat" ]; then
+      nginx_xenophobia="$(echo ${nginx_xenophobia} | tr [:lower:] [:upper:])"
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Restricting access to following country codes: ${nginx_xenophobia}"
+      {
+         echo 'geoip_country /usr/share/GeoIP/GeoIP.dat;'
+         echo 'map $geoip_country_code $allowed_country {'
+         echo '   default no;'
+         for country in ${nginx_xenophobia}; do
+            echo -e "   ${country} yes;"
+         done
+         echo '}'
+         echo
+         echo 'geo $allowed_network {'
+         echo '   default no;'
+         echo "   ${nginx_lan_ip_subnet} yes;"
+         echo "   ${docker_lan_ip_subnet} yes;"
+         echo '   64.41.200.0/24 yes;'
+         echo '}'
+      } > /etc/nginx/xenophobia.conf
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Global connections allowed"
+      {
+         echo 'geoip_country /usr/share/GeoIP/GeoIP.dat;'
+         echo 'map $geoip_country_code $allowed_country {'
+         echo '   default yes;'
+         echo '}'
+         echo 'geo $allowed_network {'
+         echo '   default yes;'
+         echo '}'
+      } > /etc/nginx/xenophobia.conf
+   fi
+}
+
+SABnzbd(){
+   if [ "${sabnzbd_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SABnzbd proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/sabnzbd.conf;$%   include /etc/nginx/locations/sabnzbd.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SABnzbd proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/sabnzbd.conf;$%   #include /etc/nginx/locations/sabnzbd.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+Deluge(){
+   if [ "${deluge_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Deluge proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/deluge.conf;$%   include /etc/nginx/locations/deluge.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Deluge proxying disabled"
+      sed -i "s%   include /etc/nginx/locations/deluge.conf;$%   #include /etc/nginx/locations/deluge.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+CouchPotato(){
+   if [ "${couchpotato_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    CouchPotatoServer proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/couchpotato.conf;$%   include /etc/nginx/locations/couchpotato.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    CouchPotatoServer proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/couchpotato.conf;$%   #include /etc/nginx/locations/couchpotato.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+SickGear(){
+   if [ "${sickgear_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SickGear proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/sickgear.conf;$%   include /etc/nginx/locations/sickgear.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    SickGear proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/sickgear.conf;$%   #include /etc/nginx/locations/sickgear.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+Headphones(){
+   if [ "${headphones_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Headphones proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/headphones.conf;$%   include /etc/nginx/locations/headphones.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Headphones proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/headphones.conf;$%   #include /etc/nginx/locations/headphones.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+Subsonic(){
+   if [ "${subsonic_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/subsonic.conf;$%   include /etc/nginx/locations/subsonic.conf;%" "/etc/nginx/conf.d/media.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Subsonic proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/subsonic.conf;$%   #include /etc/nginx/locations/subsonic.conf;%" "/etc/nginx/conf.d/media.conf"
+   fi
+}
+
+Nextcloud(){
+   if [ "${nextcloud_enabled}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Nextcloud proxying enabled"
+      sed -i "s%^   #include /etc/nginx/locations/nextcloud.conf;$%   include /etc/nginx/locations/nextcloud.conf;%" "/etc/nginx/nginx.conf"
+   else
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Nextcloud proxying disabled"
+      sed -i "s%^   include /etc/nginx/locations/nextcloud.conf;$%   #include /etc/nginx/locations/nextcloud.conf;%" "/etc/nginx/nginx.conf"
+   fi
+}
+
+SetOwnerAndGroup(){
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Correct owner and group of application files, if required"
+   if [ "${user_id}" ]; then
+      find "/etc/nginx" ! -user "${user_id}" -exec chown "${user_id}" {} \;
+   fi
+   if [ "${group_id}" ]; then 
+      find "/etc/nginx" ! -group "${group_id}" -exec chgrp "${group_id}" {} \;
+   fi
+   if [ "${user_id}" ]; then
+      find "/var/cache/nginx" ! -user "${user_id}" -exec chown "${user_id}" {} \;
+   fi
+   if [ "${group_id}" ]; then 
+      find "/var/cache/nginx" ! -group "${group_id}" -exec chgrp "${group_id}" {} \;
+   fi
+}
+
+LaunchNGINX (){
+   echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    ***** Configuration of NGINX container launch environment complete *****"
+   if [ -z "${1}" ]; then
+      echo "$(date '+%Y-%m-%d %H:%M:%S') INFO:    Starting NGINX"
+      exec "/usr/sbin/nginx"
+   else
+      exec "$@"
+   fi
+}
+
+Initialise
+DownloadFavouritesIcon
+WebProxy
+SetPassword
+ConfigureServerNames
+ConfigureCertificates
+ConfigureNextcloud
+LANLogging
+Xenophobia
+SABnzbd
+Deluge
+CouchPotato
+SickGear
+Headphones
+Subsonic
+SetOwnerAndGroup
+LaunchNGINX
